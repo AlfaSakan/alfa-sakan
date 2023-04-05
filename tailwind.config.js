555ad504c24224af688993095203461c6b0610ee
alfa <ahmad.sakan@nri.co.id>
2023-04-06 02:48:57 +0700

    feat: responsive medium and large

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundSize: {
+        "size-200": "200% 200%",
+      },
+      backgroundPosition: {
+        "pos-0": "0% 0%",
+        "pos-100": "100% 100%",
+      },
       colors: {
         aquamarine: "#64ffda",
         "aquamarine-tint": "rgba(100, 255, 218, 0.1)",
